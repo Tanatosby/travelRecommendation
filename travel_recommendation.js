7f7c4298d485f94f73dbe310efdb51508986ac2a
Tanatosby <pedro.gabriel.rotta@gmail.com>
2026-04-11 01:08:20 -0500

    advancing the logical in js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,9 +1,29 @@
 
-fetch('/travel_recommendation_api.json')
-    .then(response => response.json())
+function Searchfunc(){
+    const searching_keyword = document.getElementById('search_box').value.trim().toLowerCase();
+    if (searching_keyword == 'beach'|| searching_keyword == 'beaches'){
+        display('beaches')
+    }else if(searching_keyword == 'country'|| searching_keyword == 'countries'){
+        display('countries')
+    }else if(searching_keyword == 'temple'|| searching_keyword == 'temples'){
+        display('temples')
+    }else{
+        window.alert('You can looking for beaches, temples or countries')
+    }
+}
+
+function display(key){
+     fetch('travel_recommendation_api.json')
+    .then(response=>response.json())
     .then(data=>{
-        console.log(data);
+    let s = data[key];
+    s.forEach(element => {
+        console.log(element.name);
+    });
     })
     .catch(error=>{
-        console.error('Error', error);
+    console.log('Error:',error);
     });
+}
+
+document.getElementById('search_button').addEventListener('click',Searchfunc)
